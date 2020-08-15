5fdcf04760147e766789c7d28e0eed873b334358
Harlan Howe <hhowe7@gatech.edu>
2020-08-15 18:04:13 -0500

    Initial commit

---
+++ Greeting.py
@@ -0,0 +1,13 @@
+
+def do_greeting():
+    print("Hello!")
+    print("My name is: ______")
+    color = "blue"
+    print("edit this line with fstring to say what your favorite color is.")
+
+
+# -----------------------------------------------------------------------------------------------------------------
+# This is what the program will actually do.... like Java's main() method. (Since it's not inside the class
+#    declaration.) The "if" statement means this will only happen if this file is the one that is run.
+if __name__ == "__main__":
+    do_greeting()
